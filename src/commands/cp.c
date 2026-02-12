ff08daf8904b60e354aec8ca5e71f1ad073c737d
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2026-02-12 21:36:03 +0700

    refactor: move _start to high.h
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/commands/cp.c
+++ src/commands/cp.c
@@ -13,26 +13,3 @@ int main(int argc, char *argv[]) {
 
 	return 0;
 }
-
-__attribute__((naked)) void _start(void) {
-	// get the argc and argv from the rsp (register stack pointer)
-	// argc were placed at 0
-	// argv were placed at 8 bytes
-	__asm__ volatile (
-		".intel_syntax noprefix\n"
-		"mov rdi, [rsp]\n"
-		"lea rsi, [rsp + 8]\n"
-		"and rsp, -16\n"
-
-		"call main\n"
-
-		"mov rdi, rax\n"
-		"mov rax, 60\n"
-		"syscall\n"
-
-		".att_syntax\n"
-		:
-		:
-		: "memory", "rdi", "rsi", "rax"
-	);
-}
--- src/include/high.h
+++ src/include/high.h
@@ -206,3 +206,26 @@ static inline int fread(cstr src) {
 
 	return 0;
 }
+
+inline __attribute__((naked)) void _start(void) {
+	// get the argc and argv from the rsp (register stack pointer)
+	// argc were placed at 0
+	// argv were placed at 8 bytes
+	__asm__ volatile (
+		".intel_syntax noprefix\n"
+		"mov rdi, [rsp]\n"
+		"lea rsi, [rsp + 8]\n"
+		"and rsp, -16\n"
+
+		"call main\n"
+
+		"mov rdi, rax\n"
+		"mov rax, 60\n"
+		"syscall\n"
+
+		".att_syntax\n"
+		:
+		:
+		: "memory", "rdi", "rsi", "rax"
+	);
+}
